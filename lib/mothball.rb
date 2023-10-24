fb1c727cd95048448a96c28341ff3214f24e4e36
siranovel <siranovel@gmail.com>
2023-10-24 12:36:18 +0900

    Add files via upload

--- lib/mothball.rb
+++ lib/mothball.rb
@@ -1,10 +1,19 @@
 require 'date'
 
+# calculate after n day from today
+# (circleciによるtest&deploy)
 module MothBallLib
-  def prev_day(n)
+  # after n day from today
+  #
+  # @overload next_day(n)
+  #   @param [int] n n日後
+  #   @return [String] 今日からn日後の値(yyyy-mm-dd)
+  # @example
+  #   MothBallLib::next_day(2) => 2013-10-22
+  def next_day(n)
     today = Date.today
-    prev = today.prev_day(n)
+    prev = today.next_day(n)
     return prev.strftime("%Y-%m-%d")
   end
-  module_function :prev_day
+  module_function :next_day
 end
